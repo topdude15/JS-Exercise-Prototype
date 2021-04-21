ec6762816794cfbff069a8f0144dbd0362db05f9
Trevor Rose <topdude15@gmail.com>
2021-04-21 16:18:28 -0600

    Completed tasks in index.js file

--- index.js
+++ index.js
@@ -39,10 +39,22 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
   }
- 
+  Person.prototype.eat = function(someFood) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+ Person.prototype.poop = function() {
+   this.stomach = [];
+ }
+ Person.prototype.toString = function() {
+   return (`${this.name}, ${this.age}`);
+ }
  
 
   
@@ -63,8 +75,14 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
+ function Car(model, mpg) {
+    this.model = model;
+    this.milesPerGallon = mpg;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  Car.prototype.fill = function(gallons) {
+    this.tank += gallons;
   }
   
   
@@ -75,18 +93,25 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
+ function Baby(name, age, favoriteToy) {
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
+  }
+
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function() {
+    return (`Playing with ${this.favoriteToy}`);
   }
  
-  
+
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Global binding means that the 'this' keyword is bound to the window, not linked to any object
+    2. Implicit binding is referencing the object that comes before the period of a function call
+    3. 'This', when in new binding, refers to the object that was created
+    4. Explicit binding with 'this' is used for some JavaScript functions to override some objects
   */
   
   
